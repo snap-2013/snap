1f7618b9f659ca91441f92c41d3c919ec0d6e264
simp1eton <shencq@stanford.edu>
2013-09-26 00:54:33 -0700

    Added some comments to motifcounter.cpp

--- examples/motifcounter/motifcounter.cpp
+++ examples/motifcounter/motifcounter.cpp
@@ -5,8 +5,8 @@ int main (int argc, char *argv[]) {
   TSnap::RenumberNodes(G);
   TSnap::RemoveSelfLoops(G);
   TVec <int64> A, B;
-  TSnap::GetMotifCount(G,3,A,G->GetNodes());
-  TSnap::GetMotifCount(G,4,B,G->GetEdges());
+  TSnap::GetMotifCount(G,3,A,G->GetNodes()); //Replace G->GetNodes() with a smaller integer value if one wants to do sampling
+  TSnap::GetMotifCount(G,4,B,G->GetEdges()); //Replace G->GetEdges() with a smaller integer value if one wants to do sampling
   printf("%s,%d,%d",argv[1],G->GetNodes(),G->GetEdges());
   for(int i=0;i<2;++i) printf(",%lld",A[i]);
   for(int i=0;i<6;++i) printf(",%lld",B[i]);
